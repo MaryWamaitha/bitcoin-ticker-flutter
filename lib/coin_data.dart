d43c5f1a4a99ee6c28ad173af8db6e1513d05f90
MaryWamaitha <marywamaitha019@gmail.com>
2022-05-18 16:39:43 +0000

    learning code

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,7 @@
+import 'networking.dart';
+const apiKey = '919BB7B7-E950-4E19-A290-954B55617DF0';
+const coinApiUrl = 'https://rest.coinapi.io/v1/exchangerate';
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +32,11 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+class CoinData {
+  Future<dynamic> getCurrencyValue(coin,currency) async {
+    var url = '$coinApiUrl/$coin/$currency?apikey=$apiKey';
+    NetworkHelper networkHelper = NetworkHelper('$url');
+    var currencyData = await networkHelper.getData();
+    return currencyData;
+  }
+}
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:bitcoin_ticker/coin_data.dart';
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -6,6 +8,73 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  CoinData exchange = CoinData();
+  String selectedCurrency = 'USD';
+  String btcCoin ='';
+  String ethCoin ='';
+  String ltcCoin ='';
+  String btcCurrencyValue = '?';
+  String ethCurrencyValue = '?';
+  String ltcCurrencyValue = '?';
+
+  List<DropdownMenuItem> getDropdownItems() {
+    List<DropdownMenuItem<String>> dropdownItems = [];
+      for (int i = 0; i < currenciesList.length; i++) {
+        String currency = currenciesList[i];
+         var newItem = DropdownMenuItem(
+          child: Text(currency),
+          value: currency,
+        );
+         dropdownItems.add(newItem);
+      }
+      return dropdownItems;
+  }
+
+  void getBTCData() async{
+    btcCoin = 'BTC';
+    try {
+      var data = await exchange.getCurrencyValue(btcCoin,selectedCurrency);
+      double rate = data['rate'];
+      setState(() {
+        btcCurrencyValue= rate.toStringAsFixed(0);
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+  void getETHData() async{
+    ethCoin = 'ETH';
+    try {
+      var data = await exchange.getCurrencyValue(ethCoin,selectedCurrency);
+      double rate = data['rate'];
+      setState(() {
+        ethCurrencyValue= rate.toStringAsFixed(0);
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+  void getLTCData() async{
+    ltcCoin ='LTC';
+    try {
+      var data = await exchange.getCurrencyValue(ltcCoin,selectedCurrency);
+      double rate = data['rate'];
+      setState(() {
+        ltcCurrencyValue= rate.toStringAsFixed(0);
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getBTCData();
+    getETHData();
+    getLTCData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,36 +85,76 @@ class _PriceScreenState extends State<PriceScreen> {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  '1 BTC = ? USD',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              CurrencyBox(bitText: 'BTC', currencyValue: btcCurrencyValue, selectedCurrency: selectedCurrency),
+              CurrencyBox(bitText: 'ETH', currencyValue: ethCurrencyValue, selectedCurrency: selectedCurrency),
+              CurrencyBox(bitText: 'LTC', currencyValue: ltcCurrencyValue, selectedCurrency: selectedCurrency),
+            ],
           ),
+
+
           Container(
             height: 150.0,
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: DropdownButton<String>(
+              //VALUE is the default value that the onChanged starts with
+              value: selectedCurrency,
+              items: getDropdownItems(),
+              onChanged: (value) {
+                setState(() {
+                  selectedCurrency = value;
+                  getBTCData();
+                  getETHData();
+                  getLTCData();
+                });
+              },
+            ),
           ),
         ],
       ),
     );
   }
 }
+
+class CurrencyBox extends StatelessWidget {
+  const CurrencyBox({
+    @required this.bitText,
+    @required this.currencyValue,
+    @required this.selectedCurrency,
+  });
+
+  final String currencyValue;
+  final String selectedCurrency;
+  final String bitText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+      child: Card(
+        color: Colors.lightBlueAccent,
+        elevation: 5.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+          child: Text(
+            '$bitText = $currencyValue $selectedCurrency',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
